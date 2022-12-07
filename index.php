72ee7701a92e4ce01510f24c32cbe0044d84a1ea
Thirukandiyur Sudarshan Karthk <73633725+sudarshankarthik@users.noreply.github.com>
2022-12-07 23:40:47 +0530

    Added BootStrap

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Complaint</title>
+    <link rel="stylesheet" href="./bootstrap-5.2.3-dist/css/bootstrap.css">
 </head>
 <body>
-    
+    <?php include './src/includes/header.php'; ?>
+    <script src="./bootstrap-5.2.3-dist/js/bootstrap.js"></script>
 </body>
 </html>
--- src/includes/header.php
+++ src/includes/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-    <div>
-        <h2>E-Complaint Management System</h2>
-    </div>
-    <div>
-        <button>Login</button>
-    </div>
+    <nav>
+        <div>
+            <h2>E-Complaint Management System</h2>
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </nav>
 </body>
 </html>
