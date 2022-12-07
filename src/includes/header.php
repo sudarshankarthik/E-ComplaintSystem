b468cb1edd1509d7340a638afb25bff0d483b153
Thirukandiyur Sudarshan Karthk <73633725+sudarshankarthik@users.noreply.github.com>
2022-12-07 23:18:49 +0530

    index files created

---
+++ src/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div>
+        <h2>E-Complaint Management System</h2>
+    </div>
+    <div>
+        <button>Login</button>
+    </div>
+</body>
+</html>
--- src/login.php
+++ src/login.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Complaint</title>
+    <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <h2>Sign In</h2>
+        <form>
+            
+        </form>
+    </div>
+
+    <div>
+    </div>
 </body>
 </html>
