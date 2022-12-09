22dc63f9c3baba17ff10c2c1e35217db8efd5d9f
Thirukandiyur Sudarshan Karthk <73633725+sudarshankarthik@users.noreply.github.com>
2022-12-09 18:30:18 +0530

    Added HomePage

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    
-</head>
 <body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <div class="navbar-brand">
-            <img src="res\icon\E-Complaint.png" alt="E-Complaint" width="180"/>
+    <nav>
+        <div class="nav-logo">
+            <a href="#"><img src="res\icon\E-Complaint.png" alt="E-Complaint" width="320"></a>
         </div>
-
-        <div class="navbar-collapse" id="">
-            <a class="nav-item nav-link" href="#">Home</a>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">Pricing</a>
-            <a class="nav-item nav-link" href="#">Disabled</a>
+        <div class="nav-links">
+            
         </div>
-        <button class="btn btn-default btn-rounded btn-primary">Signin</button>
     </nav>
 </body>
 </html>     
